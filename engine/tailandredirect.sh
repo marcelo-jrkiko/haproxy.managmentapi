bbd44ef0ecc9c93ab78e9983cbce24014a11088a
Marcelo Junior <marcelojunior.tech@gmail.com>
2026-05-28 21:48:18 -0300

    fix: Correct path to gelf-redirector.py in tailandredirect.sh

--- engine/tailandredirect.sh
+++ engine/tailandredirect.sh
@@ -10,6 +10,6 @@ done
 echo "$FILE found, starting to tail."
 
 tail -F "$FILE" | while IFS= read -r message; do
-  echo "$message" | /usr/bin/python3 /app/gelf-redirector.py --log_path stdin --instance "$LOG_REDIRECTOR_INSTANCE" --client "from-host" --gelf_http_url "$GELF_HTTP_URL" --gelf_auth_type "$GELF_AUTH_TYPE" --gelf_auth_token "$GELF_AUTH_TOKEN"
+  echo "$message" | /usr/bin/python3 /app/engine/gelf-redirector.py --log_path stdin --instance "$LOG_REDIRECTOR_INSTANCE" --client "from-host" --gelf_http_url "$GELF_HTTP_URL" --gelf_auth_type "$GELF_AUTH_TYPE" --gelf_auth_token "$GELF_AUTH_TOKEN"
 done
 
